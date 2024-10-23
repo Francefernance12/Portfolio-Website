f6e5d6ae101f8a03dba5c66d867030030087bf3c
Francerfernance <thekidofsteel01@gmail.com>
2024-10-22 22:19:45 -0400

    Fix

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,26 +1,29 @@
 // Load footer from footer.html
-fetch('../../components/footer.html').then(response => {
-if (!response.ok) {
-    throw new Error('Failed to load footer');
-}
-return response.text();
-}).then(data => {
-    document.getElementById('footer-placeholder').innerHTML = data;
-})
-.catch(error => {
-    console.error('Error:', error);
-});
-
+fetch('../../components/footer.html')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Failed to load footer');
+        }
+        return response.text();
+    })
+    .then(data => {
+        document.getElementById('footer-placeholder').innerHTML = data;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
 
 // Load navbar from navbar.html
-fetch('../../components/navbar.html').then(response => {
-if (!response.ok) {
-throw new Error('Failed to load footer');
-}
-return response.text();
-}).then(data => {
-document.getElementById('navbar-placeholder').innerHTML = data;
-})
-.catch(error => {
-console.error('Error:', error);
-});
+fetch('../../components/navbar.html')
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Failed to load navbar');
+        }
+        return response.text();
+    })
+    .then(data => {
+        document.getElementById('navbar-placeholder').innerHTML = data;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
